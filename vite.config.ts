f5aeb1077f6a1568fad25a31e0c4e54a19efa7d2
TranHLong <thll160600@gmail.com>
2024-05-09 16:28:28 +0700

    vuehost

--- vite.config.ts
+++ vite.config.ts
@@ -7,7 +7,7 @@ import VueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base:'/akiaki/',
+  base:'/vuehost/',
   plugins: [
     vue(),
     vueJsx(),
