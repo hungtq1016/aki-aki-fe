14b12be3f84b977049db5a5e2e1fdbdbc4901811
hungtq1016 <hungtq1016@gmail.com>
2024-05-19 23:05:45 +0700

    update

--- src/core/services/helpers/error.helper.ts
+++ src/core/services/helpers/error.helper.ts
@@ -1,14 +1,46 @@
 import { i18n } from '@/core/services/base/translation'
 import { errorNotification, warningNotification } from './alert.helper'
+import { post } from './request.helper';
+import { useAuthInfo } from './indexedDB.helper';
+import type { TTokenResponse } from '@/core/models/type';
+import { useRouter } from 'vue-router';
 
-export const errorHandling = (
+export const errorHandling = async (
   message: string = i18n.global.t('error.server_error.title'),
   status: number
 ) => {
-  if (status === 404) {
-    warningNotification(message)
-  } else {
-    errorNotification(message)
-    throw new Error(message)
+  switch (status) {
+    case 404:
+      warningNotification(message)
+      break;
+    case 401:
+        await refreshToken()
+      break;
+    default:
+
+      errorNotification(message)
+      throw new Error(message)
+
+  }
+
+}
+
+const refreshToken = async() =>{
+  
+  const { readAuthAsync , updateAuthAsync } = useAuthInfo()
+  const token = await readAuthAsync() 
+  if (token !== undefined) {
+    await post<TTokenResponse,TTokenResponse>('/api/authenticate/refresh-token',token).then(async response =>{
+      if (response?.data) {
+        await updateAuthAsync(response.data)
+        
+      }else{
+        warningNotification("You need login")
+        window.location.replace('/')
+      }
+    })
+  }else {
+    warningNotification("You need login")
+    window.location.replace('/')
   }
 }
