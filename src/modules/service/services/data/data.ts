214224d0815d37cb94a3c85cbcac2cf32ee44d08
TranHLong <thll160600@gmail.com>
2024-05-23 22:34:40 +0700

    commit

--- src/modules/service/services/data/data.ts
+++ src/modules/service/services/data/data.ts
@@ -11,18 +11,18 @@ const tiers = ref<Array<TTier>>([
     name: 'Tiết kiệm',
     id: 'silver',
     slug: '#',
-    price: { once: '1.190.000 VNĐ', annually: '8.190.000 VNĐ' },
-    desc: 'Gói khám sức khỏe tiêu chuẩn là chương trình khám sức khỏe cơ bản, được thiết kế dành cho các khách hàng từ 12 tuổi, có nhu cầu kiểm tra sức khỏe định kỳ. ',
-    features: ['Khám Nội tổng quát', 'Công thức máu – Huyết học', 'Chức năng', 'Chẩn đoán hình ảnh    '],
+    price: { once: '800.000 VNĐ', annually: '16.000.000 VNĐ' },
+    desc: 'Gói trồng răng tiết kiệm, sử dụng implant Hàn quốc, có kiểm tra định kì. ',
+    features: ['Chụp X quang', 'Thuốc – Phẩu thuật', 'Tái khám', 'Bảo hành    '],
     mostPopular: false
   },
   {
     name: 'Cơ bản',
     id: 'tier-freelancer',
     slug: '#',
-    price: { once: '2.000.000 VNĐ', annually: '12.450.000 VNĐ' },
-    desc: 'Gói khám sức khỏe cơ bản dành cho người trưởng thành, hiệu quả, tiết kiệm chi phí, mang đến cái nhìn tổng quát nhất về sức khỏe.',
-    features: [ 'Gói tiết kiệm', 'Tầm soát tiểu đường', 'Điện giải đồ', 'Bộ xét nghiệm mỡ trong máu','Xét nghiệm xương khớp' ],
+    price: { once: '1.000.000 VNĐ', annually: '18.000.000 VNĐ' },
+    desc: 'Gói khám răng tổng quát cơ bản dành cho người trưởng thành, hiệu quả, tiết kiệm chi phí.',
+    features: [ 'Gói tiết kiệm', 'Trám răng', 'Chụp X quang', 'Cạo vôi' ],
     mostPopular: false
   },
   {
@@ -30,13 +30,13 @@ const tiers = ref<Array<TTier>>([
     id: 'tier-startup',
     slug: '#',
     price: { once: '3.880.000 VNĐ', annually: '22.150.000 VNĐ' },
-    desc: 'Gói khám sức khỏe cao cấp dành cho người trưởng thành, giúp đánh giá, tầm soát và phát hiện sớm nhiều vấn đề sức khỏe quan trọng',
+    desc: 'Gói cao cấp dành cho người trưởng thành, giúp đánh giá, phát hiện sớm những vấn đề răng',
     features: [
       'Gói cơ bản',
-      'Phân tích nước tiểu      ',
-      'Tầm soát viêm gan      ',
-      'Tầm soát ung thư      ',
-      'Tầm soát chức năng tuyến giáp      '
+      'Cạo vôi cao cấp      ',
+      'Trồng răng thẩm mỹ      ',
+      'Phẫu thuật      ',
+      'Thuốc      '
     ],
     mostPopular: true
   },
@@ -45,7 +45,7 @@ const tiers = ref<Array<TTier>>([
     id: 'tier-enterprise',
     slug: '#',
     price: { once: '5.855.000 VNĐ', annually: '40.220.000 VNĐ' },
-    desc: 'Gói khám sức khỏe toàn diện, bao gồm 57 hạng mục, xét nghiệm và chẩn đoán hình ảnh toàn diện, được thiết kế bởi các bác sĩ nhiều năm kinh nghiệm.',
+    desc: 'Gói khám răng toàn diện, toàn bộ dịch vụ răng tại phòng khám',
     features: [
       'Không giới hạn hạng mục',
       'Ưu tiên',
